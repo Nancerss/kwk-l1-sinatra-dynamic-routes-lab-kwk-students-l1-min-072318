1796dc1a2ce6bb5fed1dffcc07da39c0398d7023
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-31 19:52:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,8 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}"
   end
   
-
+  get '/:operation/:num1/:num2' do
+    
 
 end
 
