584b3cbcc3c67a79214f580e4641ccf47b6697f1
Nancerss <nancy.alquicirauc@gmail.com>
2018-07-31 19:23:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,28 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name' do 
+    @name = params[:name].reverse
+  end
+  
+  get '/square/:num' do 
+    num = params[:num].to_i
+    (num ** 2).to_s
+  end 
+  
+  get '/say/:num/:phrase' do
+    num = params[:num].to_i
+    @phrase = params[:phrase]
+    (repeat @phrase num times)
+  end
+  
+  
+  
 
 end
+
+
+
+
+
